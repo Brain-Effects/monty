da16e17ab8729c6e010ddce62c1dc3973dfaf3d9
Brain-Effects <braineffects@github.com>
2024-01-18 01:59:57 -0800

    push, pall

--- main.c
+++ main.c
@@ -2,16 +2,45 @@
 #include <stdlib.h>
 #include "monty.h"
 /**
- * main - entry point
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
  *
- * Return: always 0
+ * Description: This function opens the file specified by the user and reads it
+ * line by line. If the user does not provide exactly one argument or if the
+ * file cannot be opened, it prints an error message and exits. It also frees
+ * any memory allocated and closes the file before exiting.
+ *
+ * Return: 0 on success, EXIT_FAILURE on failure
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	stack_t *stack = NULL;
-	push(&stack, 1, "3");
-	push(&stack, 2, "5");
-	push(&stack, 3, "7");
-	pall(&stack, 4);
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+	/* Parse the line and execute the opcode */
+	}
+
+	free(line);
+	fclose(file);
 	return (0);
 }
